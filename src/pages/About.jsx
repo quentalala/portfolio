11f124b70c341bf5a6ffa9a5c23069882ec81d0e
Quentin Chin <q.chin@hotmail.com>
2022-01-12 22:41:14 -0500

    Add lorem ipsum to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -58,13 +58,21 @@ const About = () => {
                   alignItems: "center",
                   border: "1px solid lightgray",
                   height: "600px",
-                  width: "420px",
+                  width: "450px",
                   borderRadius: "5%",
                 }}
               >
                 <h3>About Me</h3>
-                <div>Paragraph goes here</div>
-                <div>Another paragraph goes here</div>
+                <div style={{ margin: "1rem 2rem 1rem" }}>
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum."
+                </div>
               </div>
             </div>
           </Grid>
